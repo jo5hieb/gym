1ca1a487d414232730e7bccef07738db3d571f83
jo5hieb <josh@theblowers.org.uk>
2021-03-23 19:00:07 +0000

    first commit

---
+++ Gym/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gym
+//
+//  Created by Joshua Blower on 23/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gym/GymApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymApp.swift
+//  Gym
+//
+//  Created by Joshua Blower on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct GymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
